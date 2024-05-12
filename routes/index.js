6ba9622880f6b88a83e1cf5688cf8b085db7d50b
Nitin gour <gournitin39@gmail.com>
2024-05-12 12:50:53 +0530

    food ordering website

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'home page' });
+});
+
+/* get menus page */
+router.get('/menus', function(req, res, next) {
+  res.render('menus', { title: 'menus page' });
+});
+
+/* get about page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+
+/* get our chefs page */
+router.get('/chefs', function(req, res, next) {
+  res.render('chefs', { title: 'our chefs' });
+});
+
+/* get contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
+
+module.exports = router;
